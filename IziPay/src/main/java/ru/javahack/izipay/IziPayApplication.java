5454cfb5f43e737f1132690f39970bf827c2c93e
FORESTER <forester2k12@bk.ru>
2019-09-14 14:24:03 +0300

    Каркас бэка (spring boot)

---
+++ IziPay/src/main/java/ru/javahack/izipay/IziPayApplication.java
@@ -0,0 +1,13 @@
+package ru.javahack.izipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IziPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IziPayApplication.class, args);
+    }
+
+}
